7de7b3b0d1d818d0d1b72cfaefb328b47a7944d9
juankcol <jamayafl15dw@ikzubirimanteo.com>
2017-05-24 12:10:53 +0000

    mini practica con jade

--- ejer-express/app.js
+++ ejer-express/app.js
@@ -4,6 +4,7 @@ var app = express();
 app.set("view engine", "jade");
 app.get("/", function(req, res){
     res.render("index",{hola: "Hola juan"});
+    console.log("Server on");
 });
 
 app.listen(8080);
