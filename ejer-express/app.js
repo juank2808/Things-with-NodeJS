2405e656b24c62558f77d34a456b55f39a460c86
juankcol <jamayafl15dw@ikzubirimanteo.com>
2017-05-24 11:25:44 +0000

    motor de vista JADE

--- ejer-express/app.js
+++ ejer-express/app.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "jade");
 app.get("/", function(req, res){
-    res.send("Hola parceros");
+    res.render("index",{hola: "Hola juan"});
 });
 
 app.listen(8080);
